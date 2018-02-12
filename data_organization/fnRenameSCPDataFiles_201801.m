88c3411216d9044889f170e2e6c186a1cbce32f7
Sebastian Moeller <moeller0@gmx.de>
2018-02-12 14:34:15 +0100

    "MOve windows helpers to AuxiliaryFunctions/Windows"

--- data_organization/fnRenameSCPDataFiles_201801.m
+++ data_organization/fnRenameSCPDataFiles_201801.m
@@ -1016,52 +1016,52 @@ return
 end
 
 
-function [ AssignedWinDriveLettersList ] = fnGetWindowsDriveLetterList()     
-    [system_status, AssignedWinDriveLetters] = system('wmic logicaldisk get caption');
-        % this might need checking for sanity
-        colon_idx = strfind(AssignedWinDriveLetters, ':');
-        AssignedWinDriveLettersList = cell(size(colon_idx));
-        for i_colon = 1 : length(colon_idx)
-            AssignedWinDriveLettersList{i_colon} = AssignedWinDriveLetters(colon_idx(i_colon)-1);
-        end
-return 
-end
+% function [ AssignedWinDriveLettersList ] = fnGetWindowsDriveLetterList()     
+%     [system_status, AssignedWinDriveLetters] = system('wmic logicaldisk get caption');
+%         % this might need checking for sanity
+%         colon_idx = strfind(AssignedWinDriveLetters, ':');
+%         AssignedWinDriveLettersList = cell(size(colon_idx));
+%         for i_colon = 1 : length(colon_idx)
+%             AssignedWinDriveLettersList{i_colon} = AssignedWinDriveLetters(colon_idx(i_colon)-1);
+%         end
+% return 
+% end
 
-function [ SubstitutedDriveLetter ] = fnSubstDrivePathToNextFreeDriveLetter(input_file_FQN, subst_finalpathcomponent_string, mode_string)
-SubstitutedDriveLetter = [];
-if (ispc)    
-    if ~exist('mode_string', 'var') || isempty(mode_string)
-        mode_string = '';
-    end
-    
-    if ~isempty(subst_finalpathcomponent_string)
-        subst_anchor_idx = strfind(input_file_FQN, subst_finalpathcomponent_string);
-        subst_anchor_string = input_file_FQN(1:(subst_anchor_idx(1) + length(subst_finalpathcomponent_string) - 1));
-    else
-        if isdir(input_file_FQN)
-            subst_anchor_string = input_file_FQN;
-        end
-    end
-    
-    PossibleDiveLetterList = {  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',...
-                                'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-      
-    for i_ProtoSubstitutedDriveLetter = 1 : length(PossibleDiveLetterList)
-        current_SubstitutedDriveLetter = [PossibleDiveLetterList{i_ProtoSubstitutedDriveLetter}, ':'];
-        [subst_status, subst_output] = system(['subst ', current_SubstitutedDriveLetter, ' ', subst_anchor_string]);
-        if (subst_status == 0)
-            % we found our candidate
-            SubstitutedDriveLetter = current_SubstitutedDriveLetter;
-            if (strcmp(mode_string, 'delete'))
-                [subst_status, subst_output] = system(['subst ', current_SubstitutedDriveLetter, ' /d']);
-            end
-            break
-        end       
-    end
-end
-
-    return
-end
+% function [ SubstitutedDriveLetter ] = fnSubstDrivePathToNextFreeDriveLetter(input_file_FQN, subst_finalpathcomponent_string, mode_string)
+% SubstitutedDriveLetter = [];
+% if (ispc)    
+%     if ~exist('mode_string', 'var') || isempty(mode_string)
+%         mode_string = '';
+%     end
+%     
+%     if ~isempty(subst_finalpathcomponent_string)
+%         subst_anchor_idx = strfind(input_file_FQN, subst_finalpathcomponent_string);
+%         subst_anchor_string = input_file_FQN(1:(subst_anchor_idx(1) + length(subst_finalpathcomponent_string) - 1));
+%     else
+%         if isdir(input_file_FQN)
+%             subst_anchor_string = input_file_FQN;
+%         end
+%     end
+%     
+%     PossibleDiveLetterList = {  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',...
+%                                 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+%       
+%     for i_ProtoSubstitutedDriveLetter = 1 : length(PossibleDiveLetterList)
+%         current_SubstitutedDriveLetter = [PossibleDiveLetterList{i_ProtoSubstitutedDriveLetter}, ':'];
+%         [subst_status, subst_output] = system(['subst ', current_SubstitutedDriveLetter, ' ', subst_anchor_string]);
+%         if (subst_status == 0)
+%             % we found our candidate
+%             SubstitutedDriveLetter = current_SubstitutedDriveLetter;
+%             if (strcmp(mode_string, 'delete'))
+%                 [subst_status, subst_output] = system(['subst ', current_SubstitutedDriveLetter, ' /d']);
+%             end
+%             break
+%         end       
+%     end
+% end
+% 
+%     return
+% end
 
 
 function [ session_start_time_HHMMSS_string ] = fnExtractEventIDEStartTimeFromReport( in_path, name_dir_wildcard, time_key_regexp, n_lines)
