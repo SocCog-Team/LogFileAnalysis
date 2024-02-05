4997d4fc8816ebc26ac45cc93a5719ed265dd05b
taskcontroller <taskcontroller@PC40687.dpz.lokal>
2024-02-05 14:01:55 +0100

    Initial commit...

---
+++ fn_get_column_name_indices_struct.m
@@ -0,0 +1,24 @@
+function [columnnames_struct, n_fields] = fn_get_column_name_indices_struct(name_list, start_val)
+% return a structure with each field for each member if the name_list cell
+% array, giving the position in the name_list, then the columnnames_struct
+% can serve as to address the columns, so the functions assigning values
+% to the columns do not have to care too much about the positions, and it
+% becomes easy to add fields.
+% name_list: cell array of string names for the fields to be added
+% start_val: numerical value to start the field values with (if empty start
+%            with 1 so the results are valid indices into name_list)
+
+if nargin < 2
+	start_val = 1;  % value of the first field
+end
+n_fields = length(name_list);
+for i_col = 1 : length(name_list)
+	cur_name = name_list{i_col};
+	% skip empty names, this allows non consequtive numberings
+	if ~isempty(cur_name)
+		columnnames_struct.(cur_name) = i_col + (start_val - 1);
+	end
+end
+return
+end
+
