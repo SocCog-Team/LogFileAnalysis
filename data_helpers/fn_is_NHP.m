df4956d95eef68439c59710435db32736413c329
taskcontroller <taskcontroller@PC40687.dpz.lokal>
2024-05-29 11:42:25 +0200

    Special case the absent None subject.

--- data_helpers/fn_is_NHP.m
+++ data_helpers/fn_is_NHP.m
@@ -15,13 +15,20 @@ macaca_mulatta_name_list = {...
 	'Bacchus', ...
 	'Pinocchio', ...
 };
-
 is_macaca_mulatta = ismember(subject_name, macaca_mulatta_name_list);
+
+none_subject_name_list = {'None'};
+is_missing_subject = ismember(subject_name, none_subject_name_list);
+
 
 if (is_macaca_mulatta)
 	short_species_string = 'NHP';
 	full_species_string = 'macaca mulatta';
 	isNHP = 1;
+elseif (is_missing_subject)
+	short_species_string = '';
+	full_species_string = '';
+	isNHP = 0;
 else
 	short_species_string = 'HP'; % human primate
 	full_species_string = 'homo sapiens';
